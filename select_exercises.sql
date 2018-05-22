b0e70b4e06565d246fc594824e6d952f3855d929
Brian Torluemke <briantorluemke@gmail.com>
2018-05-22 15:12:58 -0500

    Completed INSERT and SELECT exercises (added captions to selections using AS keyword.)

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,7 +1,6 @@
 USE codeup_test_db;
 
-SELECT 'These are the ablums by Pink Floyd' AS 'Info';
-SELECT name FROM albums WHERE artist = 'Pink Floyd';
+SELECT name AS 'Pink Floyd Albums' FROM albums WHERE artist = 'Pink Floyd';
 SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band ';
 SELECT genre FROM albums WHERE name = 'Nevermind';
 SELECT name FROM albums WHERE release_date BETWEEN 1990 AND 2000;
