70bd4bbfc59b97461bb8f845afd341285fc1c92b
Brian Torluemke <briantorluemke@gmail.com>
2018-05-23 15:40:56 -0500

    Completed exercise

--- where_exercises.sql
+++ where_exercises.sql
@@ -6,13 +6,16 @@ use employees;
 
 SELECT *
 FROM employees
-WHERE first_name = 'Irena'
-      OR first_name = 'Vidya'
-      OR first_name = 'Maya';
+WHERE (first_name = 'Irena'
+OR first_name = 'Vidya'
+OR first_name = 'Maya')
+AND gender = 'M';
 
 SELECT *
 FROM employees
-WHERE last_name LIKE 'E%';
+WHERE last_name LIKE 'E%'
+OR last_name LIKE '%e';
+
 
 SELECT *
 FROM employees
@@ -25,3 +28,13 @@ WHERE birth_date LIKE '%-12-25';
 SELECT *
 FROM employees
 WHERE last_name LIKE '%q%';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%'
+      AND last_name LIKE '%e';
+
+SELECT *
+FROM employees
+WHERE  hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+AND birth_date LIKE '%12-25';
