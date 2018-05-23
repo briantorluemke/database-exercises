1d825bbc16cf7ac13165476bbe891aa43c2a4a11
Brian Torluemke <briantorluemke@gmail.com>
2018-05-23 10:25:04 -0500

    Completed DELETE portion of statement exercise (updated for ease of access to recently modified statements.)

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,5 +1,14 @@
 USE codeup_test_db;
 
+
+SELECT 'Dropping albums release after 1991...' AS '-->INFO<--';
 DELETE FROM albums WHERE release_date > 1991;
+SELECT * FROM albums WHERE release_date > 1991;
+
+SELECT 'Dropping albums with disco genre....' AS '-->INFO<--';
 DELETE FROM albums WHERE genre = 'disco';
+SELECT * FROM albu,s WHERE genre = 'disco';
+
+SELECT 'Dropping albums by the beatles...' AS '-->INFO<--';
 DELETE FROM albums WHERE artist = 'Nirvana';
+SELECT * FROM albums WHERE artist = 'Nirvana';
