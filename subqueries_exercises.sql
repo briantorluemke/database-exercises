b9222c8be54a911a75239ff11fe61b27779e2faa
Brian Torluemke <briantorluemke@gmail.com>
2018-05-29 12:28:51 -0500

    Completed exercise 2 in Sub-Queries assignment.

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -7,3 +7,12 @@ WHERE hire_date IN (
   FROM employees
   WHERE emp_no = '101010'
 );
+
+SELECT CONCAT(first_name, ' ', last_name) 'Full Name', title
+FROM employees e
+  JOIN titles t ON e.emp_no = t.emp_no
+WHERE first_name IN (
+  SELECT first_name
+  FROM employees
+  WHERE first_name = 'Aamod'
+);
